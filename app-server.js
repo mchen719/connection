cfbbb608a4700529ae5e666ff7d1192bedb5db03
Chris Devalme <cdevalme@gmail.com>
2024-03-13 18:34:51 -0400

    app-server updated & test files added

--- app-server.js
+++ app-server.js
@@ -14,7 +14,13 @@ app.use(logger('dev'))
 app.use(favicon(path.join(__dirname, 'public', 'img','logo.png')))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api/users', require('./routes/api/userRouter'))
-// http://localhost:8000/api/todos/completed
+app.use('/api/notifications', require('./routes/api/notificationRouter'))
+app.use('/api/messages', require('./routes/api/messageRouter'))
+app.use('/api/experience', require('./routes/api/experienceRouter'))
+app.use('/api/education', require('./routes/api/educationRouter'))
+app.use('/api/post', require('./routes/api/postRouter'));
+app.use('/api/skill', require('./routes/api/skillRouter'));
+
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
