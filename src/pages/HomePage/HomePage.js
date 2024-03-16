1713750d05b5471f5852f95db2517a97bf917136
Alioune Seck <aliouneseck15@gmail.com>
2024-03-16 13:36:42 -0400

    utilities folder updated. added Logo component to homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,16 +2,18 @@ import NavBar from '../../components/NavBar/NavBar.js';
 import PostFeed from '../../components/PostFeed/PostFeed.js';
 import NewPost from '../../components/NewPost/NewPost.js';
 import SideBar from '../../components/SideBar/SideBar.js';
+import Logo from '../../components/Logo/Logo.js';
 
 export default function HomePage({}) {
 	return (
 		<>
+            <Logo />
 			<NavBar />
 
 			<div className="mainPage">
 				<div>
 					<NewPost />
-
+                        
 					<PostFeed />
 				</div>
 
