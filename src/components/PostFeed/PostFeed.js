da3285edab1c016651267176a84f6aa2a221587a
Chris Devalme <cdevalme@gmail.com>
2024-03-16 10:49:01 -0400

    functional authPage

---
+++ src/components/PostFeed/PostFeed.js
@@ -0,0 +1,16 @@
+function PostFeed() {
+  return (
+    <>
+        <div className="PostFeed">
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <CreateIcon />
+                </div>
+            </div>
+        </div>
+    
+    </>
+  )
+}
+
+export default PostFeed
---
+++ src/pages/AuthPage/AuthPage.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import styles from './AuthPage.module.scss';
+import Login from '../../components/Login/Login';
+import SignUp from '../../components/SignUp/SignUp';
+
+
+export default function AuthPage({ setUser }) {
+  const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <main className={styles.AuthPage}>
+      <div>
+        <button className={styles.h3}onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</button>
+      </div>
+      {showLogin ? <Login setUser={setUser} /> : <SignUp setUser={setUser} />}
+    </main>
+  );
+}
