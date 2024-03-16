55d1d12883cec4d2e29abf3b61e8319e51d27c0f
Alioune Seck <aliouneseck15@gmail.com>
2024-03-16 14:23:52 -0400

    removed unnecessary Logo component from homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,12 +2,10 @@ import NavBar from '../../components/NavBar/NavBar.js';
 import PostFeed from '../../components/PostFeed/PostFeed.js';
 import NewPost from '../../components/NewPost/NewPost.js';
 import SideBar from '../../components/SideBar/SideBar.js';
-import Logo from '../../components/Logo/Logo.js';
 
 export default function HomePage({}) {
 	return (
 		<>
-            <Logo />
 			<NavBar />
 
 			<div className="mainPage">
