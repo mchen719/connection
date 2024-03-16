6977a1982c9729db40ec2a48b3b05c44b08a2540
unknown <liliannekho@gmail.com>
2024-03-16 15:53:32 -0400

    jobs-api file added to utilities

--- src/components/JobListings/JobListings.js
+++ src/components/JobListings/JobListings.js
@@ -1,19 +1,19 @@
-import React from 'react';
+// import React from 'react';
 
-const Job = ({ job }) => {
-  return (
-    <div className="job">
-      <h2 className="job-title">{job.title}</h2>
-      <p className="job-company">{job.company}</p>
-      <p className="job-location">{job.location}</p>
-      <p className="job-description">{job.description}</p>
-      <ul className="job-requirements">
-        {job.requirements.map((requirement, index) => (
-          <li key={index}>{requirement}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+// const Job = ({ job }) => {
+//   return (
+//     <div className="job">
+//       <h2 className="job-title">{job.title}</h2>
+//       <p className="job-company">{job.company}</p>
+//       <p className="job-location">{job.location}</p>
+//       <p className="job-description">{job.description}</p>
+//       <ul className="job-requirements">
+//         {job.requirements.map((requirement, index) => (
+//           <li key={index}>{requirement}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
-export default Job;
+// export default Job;
