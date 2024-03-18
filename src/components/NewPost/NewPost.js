466618570aa78fd5d8a9d9d4ae589b3ce3ebfe6b
Alioune Seck <aliouneseck15@gmail.com>
2024-03-18 18:14:19 -0400

    updated HomePage.js

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -9,3 +9,36 @@ export default function NewPost({}) {
 }
 
 
+/*
+testing to see try to get form to 
+
+import React, { useState } from 'react';
+import styles from './NewPost.module.scss';
+
+export default function NewPost({ addPost }) {
+	const [post, setPost] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		addPost(post);
+		setPost('');
+	};
+
+	return (
+		<div>
+			<h1>New Post</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={post}
+					onChange={(e) => setPost(e.target.value)}
+					placeholder="Enter your post"
+				/>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
+}
+
+
+*/
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,9 +5,19 @@ import NewPost from '../../components/NewPost/NewPost.js';
 import SideBar from '../../components/SideBar/SideBar.js';
 
 export default function HomePage( user, setUser ) {
+	const [allPosts, setAllPosts] = useState([]);
+
+	useEffect(() => {
+		// Fetch all posts from the server
+		fetch('/api/posts')
+			.then(response => response.json())
+			.then(data => setAllPosts(data))
+			.catch(error => console.error(error));
+	}, []);
+
 	return (
 		<>
-		<h1>TEST HOMEPAGE TEXT </h1>
+			<h1>TEST HOMEPAGE TEXT </h1>
 			<NavBar
 				user={user}
 				setUser={setUser}
@@ -15,9 +25,22 @@ export default function HomePage( user, setUser ) {
 
 			<div className="mainPage">
 				<div>
-					<NewPost />
-                        
-					<PostFeed />
+					<NewPost>
+						{showForm && (
+							<form onSubmit={handleSubmit}>
+								<input
+									type="text"
+									value={post}
+									onChange={(e) => setPost(e.target.value)}
+									placeholder="Enter your post"
+								/>
+								<button type="submit">Submit</button>
+							</form>
+						)}
+						<button onClick={() => setShowForm(!showForm)}>New Post</button>
+					</NewPost>
+
+					<PostFeed posts={allPosts} />
 				</div>
 
 				<SideBar />
