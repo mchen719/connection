427162ac5482f42ff4b513893eabdfea2f2d4cb6
NeverGiveUpForLife32 <clflem687@yahoo.com>
2024-03-19 18:23:41 -0400

    Completed Form for NewPost

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,11 +1,54 @@
+import { useState } from 'react';
+import * as usersService from '../../utilities/users-service';
 import styles from './NewPost.module.scss';
 
-export default function NewPost({}) {
+export default function NewPostForm({}) {
+	const [credentials, setNewData] = useState({
+		title: '',
+		description: ''
+	});
+	const [error, setError] = useState('');
+	function handleChange(evt) {
+		setNewData({ ...credentials, [evt.target.name]: evt.target.value });
+		setError('');
+	}
+	async function handleSubmit(evt) {
+		// Prevent form from being submitted to the server
+		evt.preventDefault();
+		try {
+			// The promise returned by the signUp service method
+			// will resolve to the user object included in the
+			// payload of the JSON Web Token (JWT)
+			const user = await usersService.login(credentials);
+			setUser(user);
+		} catch {
+			setError('Log In Failed - Try Again');
+		}
+	}
 	return (
 		<div>
-			<h1>New Posts</h1>
+			<div className="form-container">
+				<form autoComplete="off" onSubmit={handleSubmit}>
+					<label>Title</label>
+					<input
+						type="text"
+						name="title"
+						value={credentials.title}
+						onChange={handleChange}
+						required
+					/>
+					<label>Description</label>
+					<input
+						type="text"
+						name="description"
+						value={credentials.description}
+						onChange={handleChange}
+						required
+					/>
+					<button type="submit">LOG IN</button>
+				</form>
+			</div>
+			<p className="error-message">&nbsp;{error}</p>
 		</div>
 	);
 }
-
-
