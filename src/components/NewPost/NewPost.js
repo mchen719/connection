721bff4cd9ba2b7ba35cbff930d6c2a0038220df
NeverGiveUpForLife32 <clflem687@yahoo.com>
2024-03-20 19:49:12 -0400

    Updated Form for NewPost

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,54 +1,87 @@
 import { useState } from 'react';
-import * as usersService from '../../utilities/users-service';
+import sendRequest from '../../utilities/send-request';
 import styles from './NewPost.module.scss';
 
-export default function NewPostForm({}) {
-	const [credentials, setNewData] = useState({
+// export default function NewPostForm(props) {
+// 	const [formData, setPostData] = useState({
+// 		body: ''
+// 	});
+// 	const navigateTo = useNavigate();
+// 	const handleSubmit = async (e) => {
+// 		e.preventDefault();
+// 		try {
+// 			const data = await props.createPost(formData, props.token);
+// 			// cool thing to do once there is a showpage done
+// 			navigateTo(`/post/${data._id}`);
+// 		} catch (error) {
+// 			console.error(error);
+// 		}
+// 	};
+// 	function handleChange(evt) {
+// 		setPostData({ ...credentials, [evt.target.name]: evt.target.value });
+// 		setError('');
+// 	}
+
+//Make a fetch request to backend for post route.
+
+export default function NewPost() {
+	const [formData, setFormData] = useState({
 		title: '',
 		description: ''
 	});
-	const [error, setError] = useState('');
 	function handleChange(evt) {
-		setNewData({ ...credentials, [evt.target.name]: evt.target.value });
-		setError('');
+		setFormData({ ...formData, [evt.target.name]: evt.target.value });
 	}
-	async function handleSubmit(evt) {
-		// Prevent form from being submitted to the server
-		evt.preventDefault();
-		try {
-			// The promise returned by the signUp service method
-			// will resolve to the user object included in the
-			// payload of the JSON Web Token (JWT)
-			const user = await usersService.login(credentials);
-			setUser(user);
-		} catch {
-			setError('Log In Failed - Try Again');
-		}
-	}
+
+	// return (
+	// 	<form onSubmit={handleSubmit}>
+	// 		<h2>Create A New Twitter Post {props.user.name} </h2>
+	// 		<input
+	// 			placeholder="BODY"
+	// 			type="text"
+	// 			name="body"
+	// 			value={formData.body}
+	// 			onChange={handleChange}
+	// 		/>
+	// 		<input type="submit" value="Create Post" />
+	// 	</form>
+	// );
 	return (
-		<div>
-			<div className="form-container">
-				<form autoComplete="off" onSubmit={handleSubmit}>
-					<label>Title</label>
-					<input
-						type="text"
-						name="title"
-						value={credentials.title}
-						onChange={handleChange}
-						required
-					/>
-					<label>Description</label>
-					<input
-						type="text"
-						name="description"
-						value={credentials.description}
-						onChange={handleChange}
-						required
-					/>
-					<button type="submit">LOG IN</button>
+		<>
+			<h2>Create a Post</h2>
+			<div className={styles.container}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => {
+						e.preventDefault();
+						sendRequest('/api/post', 'POST', formData);
+					}}
+				>
+					<div>
+						<label>
+							Title
+							<input
+								type="text"
+								value={formData.title}
+								name="title"
+								onChange={handleChange}
+								placeholder="Title"
+							/>
+						</label>
+						<label>
+							Description
+							<input
+								type="text"
+								value={formData.description}
+								name="description"
+								onChange={handleChange}
+								placeholder="Description"
+							/>
+						</label>
+					</div>
+					<button type="submit" value="submit" />
 				</form>
 			</div>
-			<p className="error-message">&nbsp;{error}</p>
-		</div>
+		</>
 	);
 }
