631c428df78d5a865da2419a4bcc792da182b515
unknown <liliannekho@gmail.com>
2024-03-23 15:12:39 -0400

    big fixed

--- src/utilities/jobs-api.js
+++ src/utilities/jobs-api.js
@@ -1,15 +1,3 @@
-
-// import sendRequest from './send-request';
-
-// const BASE_URL = '/api/jobs'; 
-// export function getAllJobs() {
-//   return sendRequest(BASE_URL);
-// }
-
-// export function getJobById(id) {
-//   return sendRequest(`${BASE_URL}/${id}`);
-// }
-
 import sendRequest from './send-request';
 
 const BASE_URL = '/api/jobs';
